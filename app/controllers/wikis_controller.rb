21b3327ccef16ef191c2e355b77b4b819373c525
NoNameA 774 <nonamea774@gmail.com>
2017-05-02 19:40:16 +0900

    editまでできるようにした。

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -29,20 +29,74 @@ class WikisController < ApplicationController
     @name = Name.create(name: pagename, wiki_id: @wiki.id)
     @page = Page.create(content: content, wiki_id: @wiki.id)
 
+    # つながっているnameの抽出等をここでする。
+
     redirect_to action: :show, name: pagename, status: 302
   end
 
   def show
     @name = Name.find_by(name: params[:name])
     if @name
+      # そのタイトルのページ発見。
       @wiki = @name.wiki
+      if @wiki.nil?
+        # 変更前のページタイトルなのでリダイレクトする。
+        render text: CGI.escapeHTML(@name.inspect)
+        return
+      end
       @page = @wiki.page
       render :wiki
     else
+      # なかったので新規画面を出しとく。
       @name = Name.new(name: params[:name])
       @page = Page.new(content: params[:content])
       render :new
     end
+  end
+
+  def edit
+    @name = Name.find_by(name: params[:name])
+    @wiki = @name.wiki
+    @page = @wiki.page
+    render :edit
+  end
+
+  def update
+    @name = Name.find(params.dig(:wiki, :name, :id))
+    @wiki = @name.wiki
+    if @wiki.nil?
+      # 
+    end
+    @page = @wiki.page
+
+    if params.dig(:wiki, :page, :id).to_i != @page.id
+      puts "here!!"
+      binding.pry
+      # コンフリクト発生
+    end
+
+    if (newname = params.dig(:wiki, :name, :name)) != @name.name && !newname.nil?
+      # ページタイトル変更発生！
+      oldname = @name.name
+      @name.name = newname
+      @name.save
+      @name.reload
+      Name.create!(name: oldname, changed_to: @name.id) # 古い名前のリダイレクト用エントリの作成
+      @wiki.touch
+      @wiki.save
+    end
+
+    newcontent = params.dig(:wiki, :page, :content)
+    if newcontent == @page.content
+      # 変更無し
+      redirect_to action: :show, name: @name.name, status: 302
+      return
+    end
+
+    newpage = Page.create(wiki_id: @wiki.id, content: newcontent)
+    @wiki.page = newpage
+    @wiki.save
+    redirect_to action: :show, name: @name.name, status: 302
   end
 
   private
